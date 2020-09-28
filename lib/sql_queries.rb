eb3ee9930e490cf29a5691f083bd525c51b63318
Sara A <calisaraalhindi@gmail.com>
2020-09-28 17:25:18 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,8 +42,8 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, projects.name, pledge.amount
-  FROM pledges
-  JOIN projects
+  FROM projects 
+  JOIN pledges 
   "
 end
 
