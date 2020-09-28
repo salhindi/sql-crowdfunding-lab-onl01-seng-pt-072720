6ad35737b31b82db2f931775d555a61b60dba2ab
Sara A <calisaraalhindi@gmail.com>
2020-09-28 17:27:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,6 +37,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   INNER JOIN pledges
   ON users.id = pledges.user_id
+  GROUP BY users.name
   GROUP BY SUM(pledges.amount);"
 end
 
