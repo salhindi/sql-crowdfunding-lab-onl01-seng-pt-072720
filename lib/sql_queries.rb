8b3b2179bec8da938c51d428ce4d344552554354
Sara A <calisaraalhindi@gmail.com>
2020-09-28 17:24:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, projects.name, pledge.amount
   FROM pledges
   INNER JOIN projects
-  ON projects.id = pledges.project_id"
+  "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
